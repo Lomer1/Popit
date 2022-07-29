a4bc5123aae487992368e7e965e0d7e136a51afe
Mihail <94056256+Lomer1@users.noreply.github.com>
2022-07-29 15:04:22 +0300

    Add files via upload

--- pop.mjs
+++ pop.mjs
@@ -1,5 +1,5 @@
 
-//import OrbitControls from './OrbitControls';
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 
 function tap (id) {
    let kick = document.getElementById(id)
@@ -22,17 +22,43 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 10, 13, 10);
+const geometry = {
+    cube: new THREE.BoxGeometry( 40, 40, 5),
+    sphere: new THREE.SphereGeometry(5, 64, 32, 0, 3, 0, 3.14),
+    sphere_2: new THREE.SphereGeometry(5),
+    sphere_3: new THREE.SphereGeometry(5),
+    sphere_4: new THREE.SphereGeometry(5),
+}
+ 
 
 const material = new THREE.MeshNormalMaterial();
 const mesh = new THREE.Mesh(geometry, material);
 
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const cube = new THREE.Mesh( geometry.cube, material );
+const sphere = new THREE.Mesh( geometry.sphere, material);
+const sphere_2 = new THREE.Mesh( geometry.sphere, material);
+const sphere_3 = new THREE.Mesh( geometry.sphere, material);
+const sphere_4 = new THREE.Mesh( geometry.sphere, material);
+scene.add( cube, sphere, sphere_2,sphere_3, sphere_4 );
 
-camera.position.z = 25;
+camera.position.z = 45;
+sphere.translateX(15);
+sphere.translateY(15);
 
-//var controls = new OrbitControls(camera, renderer.domElement);
+sphere_2.translateX(5);
+sphere_2.translateY(15);
+
+sphere_3.translateX(-5);
+sphere_3.translateY(15);
+
+sphere_4.translateX(-15);
+sphere_4.translateY(15);
+
+
+//this.controls = new OrbitControls(camera, domElement);
+//const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+const controls = new OrbitControls( camera, renderer.domElement );
 
 
 
@@ -42,8 +68,39 @@ camera.position.z = 25;
 function render() {
     requestAnimationFrame( render );
     //controls.update();
-    cube.rotation.x += 0.001;
-    cube.rotation.y += 0.001;
+    //cube.rotation.x += 0.01;
+    //cube.rotation.y += 0.01;
+    //sphere.rotation.x += 0.01;
+    //sphere.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
 render();
+//renderer.render(scene, camera);
+ /*
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x282c34);
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set(0,0,100); 
+camera.lookAt(0,0,0);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+const material = new THREE.MeshNormalMaterial();
+const points = [];
+points.push( new THREE.Vector3(- 10, 0, 0));
+points.push( new THREE.Vector3( 0, 10, 0));
+points.push( new THREE.Vector3( 0, 10, 10));
+points.push( new THREE.Vector3( 3, 1, 9));
+points.push( new THREE.Vector3( 0, 10, 50));
+
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line( geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);*/
+
